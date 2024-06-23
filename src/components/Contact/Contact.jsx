fae38d0f37ab947959b6eb275045c510fd4fbe74
sahibashahab <92646376+sahibashahab@users.noreply.github.com>
2024-06-23 22:14:43 +0500

    Add files via upload

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="sahibaawan123@gmail.com">sahibaawan123@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="linkedin.com/in/sahiba-awan-71b735219">linkedin.com/sahiba-awan</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/sahibashahab">github.com/sahibashahab</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
